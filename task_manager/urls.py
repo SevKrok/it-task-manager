7367fa5937e4312d9c058634e8ef9ac276b66de9
Severyn Krok <sevkrok@gmail.com>
2023-11-23 19:48:35 +0200

    Add CRUD for workers and tasks

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,15 +1,29 @@
 from django.urls import path
 
-from task_manager.views import index, TaskListView, WorkerListView, WorkerDetailView, TaskDetailView
+from task_manager.views import (index,
+                                TaskListView,
+                                WorkerListView,
+                                WorkerDetailView,
+                                TaskDetailView,
+                                WorkerCreateView,
+                                WorkerUpdateView,
+                                TaskUpdateView,
+                                TaskDeleteView,
+                                TaskCreateView)
 
 urlpatterns = [
     path("", index, name="index"),
 
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("workers/create/", WorkerCreateView.as_view(), name="worker-create"),
+    path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
 
 ]
 
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
-from task_manager.forms import WorkerSearchForm
+from task_manager.forms import WorkerSearchForm, WorkerCreationForm, WorkerUpdateForm, TaskUpdateForm, TaskSearchForm, \
+    TaskCreationForm
 from task_manager.models import Task, Worker
 
 
@@ -23,9 +25,28 @@ def index(request):
 
 class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
-    paginate_by = 5
     queryset = Task.objects.select_related("task_type").prefetch_related("assignees")
     ordering = ["deadline"]
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        task_type = self.request.GET.get("task_type", "")
+        context["search_form"] = TaskSearchForm(
+            initial={
+                "task_type": task_type
+            }
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        form = TaskSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                task_type__name__icontains=form.cleaned_data["task_type"]
+            )
+        return queryset
 
 
 class TaskDetailView(LoginRequiredMixin, generic.DetailView):
@@ -33,10 +54,27 @@ class TaskDetailView(LoginRequiredMixin, generic.DetailView):
     queryset = Task.objects.select_related("task_type").prefetch_related("assignees")
 
 
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    form_class = TaskCreationForm
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    form_class = TaskUpdateForm
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task_manager:task-list")
+
+
 class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
-    paginate_by = 5
     queryset = Worker.objects.select_related("position")
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -61,3 +99,15 @@ class WorkerListView(LoginRequiredMixin, generic.ListView):
 class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
     model = Worker
     queryset = Worker.objects.select_related("position").prefetch_related("tasks")
+
+
+class WorkerCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Worker
+    form_class = WorkerCreationForm
+    success_url = reverse_lazy("task_manager:worker-list")
+
+
+class WorkerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Worker
+    form_class = WorkerUpdateForm
+    success_url = reverse_lazy("task_manager:index")
