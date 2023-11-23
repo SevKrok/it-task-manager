613d83b2c64fc4691e8195e8e656d0812b281400
Severyn Krok <sevkrok@gmail.com>
2023-11-23 14:23:18 +0200

    Add Home page and pages with all workers and all tasks

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task_manager.forms import WorkerSearchForm
+from task_manager.models import Task, Worker
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    paginate_by = 5
+    queryset = Task.objects.select_related("task_type").prefetch_related("assignees")
+    ordering = ["deadline"]
+
+
+class WorkerListView(LoginRequiredMixin, generic.ListView):
+    model = Worker
+    paginate_by = 5
+    queryset = Worker.objects.select_related("position")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = WorkerSearchForm(
+            initial={
+                "username": username
+            }
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Worker.objects.all()
+        form = WorkerSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+        return queryset
